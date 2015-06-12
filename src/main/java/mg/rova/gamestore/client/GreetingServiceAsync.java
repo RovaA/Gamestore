adc5c3bc2b0518e36fc16161ff44f705e2674be3
RovaA <rovaa@userpc.com>
2015-06-12 07:16:06 +0300

    [master] INIT project initialization

---
+++ src/main/java/mg/rova/gamestore/client/GreetingServiceAsync.java
@@ -0,0 +1,9 @@
+package mg.rova.gamestore.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface GreetingServiceAsync {
+
+	void greetServer(String name, AsyncCallback<String> callback);
+
+}
---
+++ src/main/java/mg/rova/gamestore/client/Messages.java
@@ -0,0 +1,10 @@
+package mg.rova.gamestore.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
